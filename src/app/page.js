b1c38cafb6d4040d52e44ca7465596040c1630e8
GokdenizCakir <gokdenizckr@gmail.com>
2024-01-25 17:27:30 +0300

    url changed to vercel

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import Image from 'next/image';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
   const isInitialRender = useRef(true);
@@ -19,18 +19,18 @@ export default function Home() {
     const fetchOngoingEvents = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:3000/api/ongoingevents"
+          'https://oda-skyl-app.vercel.app/api/ongoingevents'
         );
         setOngoingEvents(response.data.ongoing_events || []);
       } catch (error) {
-        console.error("Error fetching ongoing events:", error);
+        console.error('Error fetching ongoing events:', error);
       }
     };
 
     const fetchNextEvents = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:3000/api/nextevents"
+          'https://oda-skyl-app.vercel.app/api/nextevents'
         );
         const allNextEvents = response.data.next_events || [];
 
@@ -46,7 +46,7 @@ export default function Home() {
 
         setNextEvents(filteredNextEvents);
       } catch (error) {
-        console.error("Error fetching next events:", error);
+        console.error('Error fetching next events:', error);
       }
     };
 
@@ -92,59 +92,59 @@ export default function Home() {
   };
 
   const renderEventCard = (event) => (
-    <div key={event.id} className="bg-white rounded shadow p-4">
-      <p className="font-bold text-xl mb-2">{event.summary}</p>
-      <p className="text-gray-600">
-        <span className="font-bold">Start Time:</span>{" "}
+    <div key={event.id} className='bg-white rounded shadow p-4'>
+      <p className='font-bold text-xl mb-2'>{event.summary}</p>
+      <p className='text-gray-600'>
+        <span className='font-bold'>Start Time:</span>{' '}
         {new Date(event.start.dateTime).toLocaleTimeString()}
       </p>
-      <p className="text-gray-600">
-        <span className="font-bold">End Time:</span>{" "}
+      <p className='text-gray-600'>
+        <span className='font-bold'>End Time:</span>{' '}
         {new Date(event.end.dateTime).toLocaleTimeString()}
       </p>
     </div>
   );
 
   return (
-    <div className="dark">
-      <div className="h-screen transition-all relative font-inter flex items-center justify-center bg-white dark:bg-darkBlue">
+    <div className='dark'>
+      <div className='h-screen transition-all relative font-inter flex items-center justify-center bg-white dark:bg-darkBlue'>
         <a
-          href="http://yildizskylab.com"
-          className="absolute p-6 cursor-pointer tracking-[0.25rem] text-xl sm:-rotate-90 left-6 sm:left-2 top-8 sm:top-16 font-bebasNeue text-darkBlue dark:text-white"
+          href='http://yildizskylab.com'
+          className='absolute p-6 cursor-pointer tracking-[0.25rem] text-xl sm:-rotate-90 left-6 sm:left-2 top-8 sm:top-16 font-bebasNeue text-darkBlue dark:text-white'
         >
           SKY LAB
         </a>
 
-        <section className="flex flex-col justify-between items-center">
-          <div className="flex flex-col items-center justify-center">
+        <section className='flex flex-col justify-between items-center'>
+          <div className='flex flex-col items-center justify-center'>
             {roomStatus.isEmpty == null && (
               <>
-                <span className="loader"></span>
-                <h1 className="text-darkBlue dark:text-white mt-6 font-bold text-4xl sm:text-5xl md:text-7xl">
+                <span className='loader'></span>
+                <h1 className='text-darkBlue dark:text-white mt-6 font-bold text-4xl sm:text-5xl md:text-7xl'>
                   Yükleniyor...
                 </h1>
               </>
             )}
             {roomStatus.isEmpty != null && (
-              <div className="bg-white flex justify-between items-center w-60 sm:w-80 aspect-square overflow-hidden rounded-[50%]">
+              <div className='bg-white flex justify-between items-center w-60 sm:w-80 aspect-square overflow-hidden rounded-[50%]'>
                 <Image
-                  src={`/${roomStatus.isEmpty ? "happy" : "sad"}.svg`}
-                  alt="Skylab"
+                  src={`/${roomStatus.isEmpty ? 'happy' : 'sad'}.svg`}
+                  alt='Skylab'
                   width={0}
                   height={0}
-                  sizes="100vw"
-                  style={{ width: "100%", height: "auto" }}
+                  sizes='100vw'
+                  style={{ width: '100%', height: 'auto' }}
                 />
               </div>
             )}
             {roomStatus.isEmpty == true && (
-              <h1 className="text-darkBlue dark:text-white mt-6 font-bold text-4xl sm:text-5xl md:text-7xl">
+              <h1 className='text-darkBlue dark:text-white mt-6 font-bold text-4xl sm:text-5xl md:text-7xl'>
                 Oda şu an boş.
               </h1>
             )}
             {roomStatus.isEmpty == false && (
-              <p className="mt-2 md:mt-6 w-80 md:w-96 font-medium text-lg md:text-xl text-center text-darkBlue dark:text-white">
-                Şu anda: {roomStatus.eventName} etkinliği mevcut. En yakın{" "}
+              <p className='mt-2 md:mt-6 w-80 md:w-96 font-medium text-lg md:text-xl text-center text-darkBlue dark:text-white'>
+                Şu anda: {roomStatus.eventName} etkinliği mevcut. En yakın{' '}
                 {new Date(roomStatus.endTime).toLocaleTimeString()} saatinde
                 kullanılabilir.
               </p>
@@ -152,10 +152,10 @@ export default function Home() {
           </div>
         </section>
         <a
-          href="https://calendar.google.com/calendar/embed?src=bc6edfd480bce7cd796fdc737fb81ea97053ac5d51208e1cff1f04bb46168f1b%40group.calendar.google.com&ctz=Europe%2FIstanbul"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="absolute bottom-16 sm:bottom-12 py-4 px-20 md:hover:scale-110 transition-transform flex justify-center items-center text-white dark:text-darkBlue bg-darkBlue dark:bg-white"
+          href='https://calendar.google.com/calendar/embed?src=bc6edfd480bce7cd796fdc737fb81ea97053ac5d51208e1cff1f04bb46168f1b%40group.calendar.google.com&ctz=Europe%2FIstanbul'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='absolute bottom-16 sm:bottom-12 py-4 px-20 md:hover:scale-110 transition-transform flex justify-center items-center text-white dark:text-darkBlue bg-darkBlue dark:bg-white'
         >
           Takvime Git
         </a>
